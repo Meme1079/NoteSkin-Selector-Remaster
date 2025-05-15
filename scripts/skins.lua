260a3c66a59bd95a4c58501d81bb12ede9a23dfb
Meme1079 <101881784+Meme1079@users.noreply.github.com>
2025-05-15 16:48:13 +0000

    skins rgb thingy

--- scripts/skins.lua
+++ scripts/skins.lua
@@ -42,11 +42,12 @@ end
 
 --- 
 ---@param skinStaticDataType table
+---@param skinMetadataType table
 ---@param strumData string
 ---@return
-local function skinsMetadataObjectData(skinStaticDataType, strumData)
-     local skinMetadataObject         = getNoteSkinMetadataPlayer
-     local skinMetadataObjectByAnim   = getNoteSkinMetadataPlayer.strums
+local function skinsMetadataObjectData(skinStaticDataType, skinMetadataType, strumData)
+     local skinMetadataObject         = skinMetadataType
+     local skinMetadataObjectByAnim   = skinMetadataType.strums
      local skinStaticDataObjectByAnim = skinStaticDataType.strums
      if skinMetadataObject == '@void' or skinMetadataObjectByAnim == nil then
           return skinStaticDataObjectByAnim[strumData]
@@ -64,45 +65,64 @@ end
 
 ---
 ---@param skinStaticDataType table
+---@param skinMetadataType table
 ---@param element string
 ---@return any
-local function skinsMetadataObjects(skinStaticDataType, element)
-     local skinsMetadataObject       = getNoteSkinMetadataPlayer
-     local skinsMetadataObjectByElem = getNoteSkinMetadataPlayer[element]
+local function skinsMetadataObjects(skinStaticDataType, skinMetadataType, element)
+     local skinsMetadataObject       = skinMetadataType
+     local skinsMetadataObjectByElem = skinMetadataType[element]
      if skinsMetadataObject == '@void' or skinsMetadataObjectByElem == nil then
           return skinStaticDataType[element]
      end
      return skinsMetadataObjectByElem
 end
 
+local skinMetadataObjectNotes = {
+     player = {
+          strums    = {
+               height  = skinsMetadataObjectData(skinStaticDataNotes, getNoteSkinMetadataPlayer, 'height'),
+               offsetX = skinsMetadataObjectData(skinStaticDataNotes, getNoteSkinMetadataPlayer, 'offsetX')
+          },
+          types     = skinsMetadataObjects(skinStaticDataNotes, getNoteSkinMetadataPlayer, 'types'),
+          rgbshader = skinsMetadataObjects(skinStaticDataNotes, getNoteSkinMetadataPlayer, 'rgbshader')
+     },
+     opponent = {     
+          strums    = {
+               height  = skinsMetadataObjectData(skinStaticDataNotes, getNoteSkinMetadataOpponent, 'height'),
+               offsetX = skinsMetadataObjectData(skinStaticDataNotes, getNoteSkinMetadataOpponent, 'offsetX')
+          },
+          types     = skinsMetadataObjects(skinStaticDataNotes, getNoteSkinMetadataOpponent, 'types'),
+          rgbshader = skinsMetadataObjects(skinStaticDataNotes, getNoteSkinMetadataOpponent, 'rgbshader')
+     }
+}
+local skinMetadataObjectSplashes = {
+     types     = skinsMetadataObjects(skinStaticDataSplashes, getSplashesSkinMetadataPlayer, 'types'),
+     rgbshader = skinsMetadataObjects(skinStaticDataSplashes, getSplashesSkinMetadataPlayer, 'rgbshader')
+}
 
+local skinMetadataObjectNotePlayer   = skinMetadataObjectNotes.player
+local skinMetadataObjectNoteOpponent = skinMetadataObjectNotes.opponent
+local fliterSkinNotePathPlayer       = filterSkinImagePath( getNoteSkinImagePathPlayer )
+local fliterSkinNotePathOpponent     = filterSkinImagePath( getNoteSkinImagePathOpponent )
+
+local skinMetadataObjectSplashPlayer = skinMetadataObjectSplashes
+local filterSkinSplashesPathPlayer   = filterSkinImagePath( getSplashesSkinImagePathPlayer )
 function onCreatePost()
-     local fliterSkinNotePathPlayer   = filterSkinImagePath( getNoteSkinImagePathPlayer )
-     local fliterSkinNotePathOpponent = filterSkinImagePath( getNoteSkinImagePathOpponent )
-
-     debugPrint( skinsMetadataObjectData(skinStaticDataNotes, 'height') )
-     debugPrint( skinsMetadataObjects(skinStaticDataNotes, 'rgbshader') )
-
-     
      for strums = 0,3 do
           setPropertyFromGroup('playerStrums', strums, 'texture', fliterSkinNotePathPlayer)
-          setPropertyFromGroup('playerStrums', strums, 'useRGBShader', false)
+          setPropertyFromGroup('playerStrums', strums, 'useRGBShader', skinMetadataObjectNotePlayer.rgbshader)
 
           setPropertyFromGroup('opponentStrums', strums, 'texture', fliterSkinNotePathOpponent)
-          setPropertyFromGroup('opponentStrums', strums, 'useRGBShader', false)
+          setPropertyFromGroup('opponentStrums', strums, 'useRGBShader', skinMetadataObjectNoteOpponent.rgbshader)
      end
 
-     local filterSkinSplashesPathPlayer = filterSkinImagePath( getSplashesSkinImagePathPlayer )
      for memberIndex = 0, getProperty('unspawnNotes.length')-1 do
           setPropertyFromGroup('unspawnNotes', memberIndex, 'noteSplashData.texture', filterSkinSplashesPathPlayer)
-          setPropertyFromGroup('unspawnNotes', memberIndex, 'noteSplashData.useRGBShader', false)
+          setPropertyFromGroup('unspawnNotes', memberIndex, 'noteSplashData.useRGBShader', skinMetadataObjectSplashPlayer.rgbshader)
      end
 end
 
 function onSpawnNote(memberIndex, noteData, noteType, isSustainNote, strumTime)
-     --[[ local fliterSkinNotePathPlayer   = filterSkinImagePath( getNoteSkinImagePathPlayer )
-     local fliterSkinNotePathOpponent = filterSkinImagePath( getNoteSkinImagePathOpponent )
-
      local songSpeed    = getProperty('songSpeed')
      local songPlayRate = getProperty('playbackRate')
      local noteSustainHeight = ((stepCrochet / 100 * 1.05) * songSpeed) / songPlayRate
@@ -110,31 +130,40 @@ function onSpawnNote(memberIndex, noteData, noteType, isSustainNote, strumTime)
      local ultimateSwagWidth = getPropertyFromClass('objects.Note', 'swagWidth')
      local ultimateNoteWidth = getPropertyFromGroup('notes', memberIndex, 'width')
      local ultimateWidth     = (ultimateSwagWidth - ultimateNoteWidth) / 2
-     if getPropertyFromGroup('notes', memberIndex, 'mustPress') then
-          setPropertyFromGroup('notes', memberIndex, 'texture', fliterSkinNotePathPlayer);
 
-          if isSustainNote == true then
-               setPropertyFromGroup('notes', memberIndex, 'offsetX', ultimateNoteWidth - getNoteSkinMetadataPlayer.strums.offsetX)
-
-               local isTailNote = stringEndsWith(getProperty('game.notes.members['..memberIndex..'].animation.curAnim.name'), 'end')
-               if not isTailNote then
-                    setPropertyFromGroup('notes', memberIndex, 'scale.y', noteSustainHeight / getNoteSkinMetadataPlayer.strums.height % noteSustainHeight)
+     ---
+     ---@param skinMetadataObjectType table
+     ---@return nil
+     local function setPropertySustain(skinMetadataObjectType)
+          if isSustainNote == false then
+               return
+          end
+          local sustainNoteOffsetX = ultimateNoteWidth - skinMetadataObjectType.strums.offsetX
+          setPropertyFromGroup('notes', memberIndex, 'offsetX', sustainNoteOffsetX)
+          
+          local isTailName = getProperty('game.notes.members['..memberIndex..'].animation.curAnim.name')
+          local isTailNote = stringEndsWith(isTailName, 'end')
+          if not isTailNote then
+               local sustainNoteHeight = noteSustainHeight / skinMetadataObjectType.strums.height % noteSustainHeight
+               if skinMetadataObjectType.strums.height == 0 then --! IF NO VALUE ADDED; DO NOT DELETE 
+                    setPropertyFromGroup('notes', memberIndex, 'scale.y', noteSustainHeight)
+               else
+                    setPropertyFromGroup('notes', memberIndex, 'scale.y', sustainNoteHeight)
                end
           end
+     end
+
+     if getPropertyFromGroup('notes', memberIndex, 'mustPress') then
+          setPropertyFromGroup('notes', memberIndex, 'texture', fliterSkinNotePathPlayer);
+          setPropertyFromGroup('notes', memberIndex, 'rgbShader.enabled', skinMetadataObjectNotePlayer.rgbshader)
+          setPropertySustain(skinMetadataObjectNotePlayer)
           updateHitboxFromGroup('notes', memberIndex)
      else
           setPropertyFromGroup('notes', memberIndex, 'texture', fliterSkinNotePathOpponent);
-
-          if isSustainNote == true then
-               setPropertyFromGroup('notes', memberIndex, 'offsetX', ultimateNoteWidth - getNoteSkinMetadataOpponent.strums.offsetX)
-
-               local isTailNote = stringEndsWith(getProperty('game.notes.members['..memberIndex..'].animation.curAnim.name'), 'end')
-               if not isTailNote then
-                    setPropertyFromGroup('notes', memberIndex, 'scale.y', noteSustainHeight / getNoteSkinMetadataOpponent.strums.height % noteSustainHeight)
-               end
-          end
+          setPropertyFromGroup('notes', memberIndex, 'rgbShader.enabled', skinMetadataObjectNoteOpponent.rgbshader)
+          setPropertySustain(skinMetadataObjectNoteOpponent)
           updateHitboxFromGroup('notes', memberIndex)
-     end ]]
+     end
 end
 
 function onUpdatePost(elapsed)
